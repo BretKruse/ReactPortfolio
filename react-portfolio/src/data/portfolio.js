e1499f53bcb32a76ba4352f86b45ff52cb9ef068
BretKruse <krusebret@gmail.com>
2023-11-27 23:26:00 -0600

    Contact and Timeline

--- react-portfolio/src/data/portfolio.js
+++ react-portfolio/src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
-        title: 'Terminal Website',
-        imgUrl: '',
+        title: 'NoteTaker',
+        imgUrl: '/assets/NoteTaker.png',
         stack: ['HTML', 'CSS', 'JavaScript'],
-        link: 'https://github.com/BretKruse/student-portfolio.git',
+        link: 'https://github.com/BretKruse/NoteTaker.git',
     },
     {
-        title: 'React Portfolio',
-        imgUrl: '',
-        stack: ['React', 'Tailwind', 'Vite'],
-        link: 'https://github.com/BretKruse/ReactPortfolio.git',
+        title: 'Tech Blog',
+        imgUrl: '/assets/TechBlog.png',
+        stack: ['HTML', 'JavaScript', 'MySQL'],
+        link: 'https://github.com/BretKruse/TechBlog.git',
     },
     {
         title: 'Book Search',
-        imgUrl: '',
+        imgUrl: '/assets/BookSearch.png',
         stack: ['React', 'CSS', 'Vite'],
         link: 'https://github.com/BretKruse/MernBookSearch.git',
     },
---
+++ react-portfolio/src/data/timeline.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        year: 'Today',
+        title: 'Product Technical Support',
+        duration: '2 years',
+        details: 'My current job position consists of working with hardware, firmware, and software to support the field technicians in the company and create document and read log data to help diagnose issues'
+    },
+    {
+        year: 'Today',
+        title: 'Web Developer',
+        duration: '6 months',
+        details: 'I have taken a 6 month boot camp to learn Web Development and we have worked with HTML, CSS, JavaScript, NodeJS, MySQL, MongoDB(NoSQL), React, and Vite'
+    },
+    {
+        year: '2019',
+        title: 'IT Network',
+        duration: '2 years',
+        details: 'A 2 year course of Madison Area Technical College to learn networking. I have worked with Linux and Windows systems. Mainly programmed Cisco routers and switches and added VoIP. Other programs I have worked with is Firewall, Python, Wireless routers, and Windows Server'
+    }
+]
